88e2bb1bf1c6720830f313a213be13de86047512
Alex <alex.mahinko@gmail.com>
2017-05-06 18:37:41 -0400

    Cat pictures now populate on page (and will populat as many cats as there are divs with valid ID's)

--- cats.js
+++ cats.js
@@ -2,8 +2,26 @@ console.log("ready")
 $(document).ready( function () {
 
   $(".summon-cats").click(function() {
-    console.log("Handler in place.")
-  });
+    $.ajax({
+      url: 'http://bitkittens.herokuapp.com/cats.json',
+      method: 'get',
+      data: {},
+      datatype: 'json',
+    }).done( function (responseData){
+      console.log("request finished");
+      counter = 0
+      var kitties = responseData['cats'];
+      while (counter < kitties.length) {
+        console.log(counter);
+        catUrl = kitties[counter]['photo'];
+        $('#cat'+(counter+1)).html('<img src='+catUrl+'>')
+        counter ++;
+      }
+    }
 
+  );
+
+
+});
 
 });
